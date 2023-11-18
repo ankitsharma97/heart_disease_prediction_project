7be405617aa8173f38fe7c2b6770f99d290a4a3e
ankitsharma97 <ankit972125@gmail.com>
2023-11-18 12:28:46 +0530

    just

--- prediction_app/models.py
+++ prediction_app/models.py
@@ -2,8 +2,6 @@
 from django.db import models
 
 class UserInput(models.Model):
-    first_name = models.CharField(max_length=50, default='')
-    last_name = models.CharField(max_length=50, default='')
     age = models.IntegerField()
     sex = models.IntegerField()
     cp = models.IntegerField()
@@ -18,8 +16,6 @@ class UserInput(models.Model):
     ca = models.IntegerField()
     thal = models.IntegerField()
 
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
 
 class Prediction(models.Model):
     user_input = models.ForeignKey(UserInput, on_delete=models.CASCADE)
